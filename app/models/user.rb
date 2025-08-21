8a0d5717cd6d803c353228338e941fc41fcf82c6
ricardo <nishimu75618094@gmail.com>
2025-08-21 20:39:14 +0900

    show.new,create,action

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :boards, dependent: :destroy
+
+  def has_written?(article)
+    boards.exists?(id: article.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root to: "home#index"
+  root to: 'boards#index'
+
+  resources :boards
 end
